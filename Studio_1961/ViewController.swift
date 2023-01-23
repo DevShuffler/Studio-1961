64aa2fa34d0be556a2e6d0e468e5f5813c9e931c
Antonov Vasiliy <ContDevAV@gmail.ru>
2023-01-23 14:38:42 +0400

    Initial commit

--- Studio_1961/ViewController.swift
+++ Studio_1961/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    var playing = false
+    
+    @IBOutlet weak var BackgroundEffect: UIImageView!
+    
+    
+    @IBAction func playButton(_ sender: Any) {
+        if (!playing) {
+            player?.play()
+            playing = !playing
+        }
+    }
+    
+    
+    @IBAction func pauseButton(_ sender: Any) {
+        if (playing) {
+            player?.pause()
+            playing = !playing
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        UIView.animate(withDuration: 30) {
+            self.BackgroundEffect.transform = CGAffineTransform(rotationAngle: .pi)
+        }
+        
+        let url = URL(string: "https://swifteasy.ru/lessons/App003/start.mp3")
+        let data = try? Data (contentsOf: url!)
+        
+        do {
+            player = try AVAudioPlayer(data: data!)
+        }
+        catch{
+        }
     }
-
-
 }
 
